ed08d09433ad913f27be71f2825da881052988a5
youknowwhatimean <sovetov.ermat@mail.ru>
2026-01-28 23:46:38 +0600

    чекпоинт

---
+++ aula-site/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "AULA F75 | The Art of Typing",
+    description: "Experience the AULA F75 Mechanical Keyboard.",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
